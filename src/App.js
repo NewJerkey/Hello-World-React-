33da16966148f473bd06875fee677600cb76cf67
NewJerkey <122385464+NewJerkey@users.noreply.github.com>
2023-02-07 12:44:13 -0500

    Oops

--- src/App.js
+++ src/App.js
@@ -3,22 +3,34 @@ import './App.css';
 
 function App() {
   return (
-    <>
-      <div>
-        <button class="buttons">
+    <><div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello World!
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div><button class="buttons">
         Duplicate</button><button id="changeBackground" class="buttons">Change Background Color</button><button id="changeTitle" class="buttons">Change Title</button><button id="delete" class="buttons">Delete Last Instance</button><div class="card" id="basecard">
         <h2 id="title" class="header">
           Bryce Schneider
         </h2>
-        <img src={logo} className="image" alt="logo" />
+        <img class="image" src https />://cdn.discordapp.com/attachments/1062806966021402684/1063160172773654659/IMG_9420.png anchor=center&mode=crop&width=1920&height=1080&rnd=132303317085370000 width=250px&gt;
         <p class="paragraph">
           This image depicts a strapping young Penn State student reacting to an insane moment in his favorite class: IST 256. He can not believe what he is seeing!
         </p>
         <button class="button" id="details">
           Details
         </button>
-      </div>
-  </>  
+      </div></>  
   
   );
 }
