cdda442c9e4ac7cc3800d08b67185124188ab262
NewJerkey <122385464+NewJerkey@users.noreply.github.com>
2023-02-08 16:59:31 -0500

    HTML + CSS working
    Old template cleaned out and card HTML left over.

--- src/App.js
+++ src/App.js
@@ -4,20 +4,7 @@ import './App.css';
 function App() {
   return (
     <><div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div><button class="buttons">
         Duplicate</button><button id="changeBackground" class="buttons">Change Background Color</button><button id="changeTitle" class="buttons">Change Title</button><button id="delete" class="buttons">Delete Last Instance</button><div class="card" id="basecard">
         <h2 id="title" class="header">
@@ -30,6 +17,7 @@ function App() {
         <button class="button" id="details">
           Details
         </button>
+
       </div></>  
   
   );
