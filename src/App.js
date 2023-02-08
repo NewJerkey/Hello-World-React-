6d7ecc270dc1824baa0817a4d8320bb8e79e9dfb
NewJerkey <122385464+NewJerkey@users.noreply.github.com>
2023-02-08 18:11:41 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,22 +4,30 @@ import './App.css';
 function App() {
   return (
     <><div className="App">
-      
-    </div><button class="buttons">
-        Duplicate</button><button id="changeBackground" class="buttons">Change Background Color</button><button id="changeTitle" class="buttons">Change Title</button><button id="delete" class="buttons">Delete Last Instance</button><div class="card" id="basecard">
-        <h2 id="title" class="header">
+
+    </div>
+      <div class="buttons">
+        <button class="copy">Duplicate</button>
+        <button class="changeBackground">Change Background</button>
+        <button class="changeTitle">Change Title</button>
+        <button id="delete">Delete
+          Last Instance</button>
+      </div>
+      <div class="card">
+        <h2 class="header">
           Bryce Schneider
         </h2>
         <img src={logo} class="image" alt="Bryce Schneider" />
         <p class="paragraph">
           This image depicts a strapping young Penn State student reacting to an insane moment in his favorite class: IST 256. He can not believe what he is seeing!
         </p>
-        <button class="button" id="details">
+        <button class="details">
           Details
         </button>
+      </div>
 
-      </div></>  
-  
+    </>
+
   );
 }
 
